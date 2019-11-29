d07ec7d42b5de10fbf5cb3aeeea0c9b13c211e79
Rigel Glen <rigelglen@gmail.com>
2019-11-29 20:24:56 +0530

    Updates

---
+++ precache-manifest.ecb06cfc1897c37739344f8916bc7493.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6f792b265793936ef071cc2b75f2eca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3d737c57b8b306e5227",
+    "url": "/static/css/main.75258064.chunk.css"
+  },
+  {
+    "revision": "2a5f96234df0c6dbbb90",
+    "url": "/static/js/2.5a96c690.chunk.js"
+  },
+  {
+    "revision": "d3d737c57b8b306e5227",
+    "url": "/static/js/main.71ec9b4d.chunk.js"
+  },
+  {
+    "revision": "84d6721e64a18fe343e6",
+    "url": "/static/js/runtime-main.f47b6839.js"
+  }
+]);
