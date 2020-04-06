89485021b5c75832013f03b0fc4d27301346f508
Rigel <rigelglen@gmail.com>
2020-04-06 10:50:53 +0530

    Updates

---
+++ precache-manifest.d40469d375f7d17dbeddcb29266a00c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88529bed992208881893072499fe71ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4e3cd6d6187e7f91a8ba",
+    "url": "./static/css/main.75ca781e.chunk.css"
+  },
+  {
+    "revision": "c8f6f4d1bc2d6f6d624b",
+    "url": "./static/js/2.28493bb0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.28493bb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e3cd6d6187e7f91a8ba",
+    "url": "./static/js/main.4f18182e.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "./static/js/main.4f18182e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8281b323063a3ee29fb3",
+    "url": "./static/js/runtime-main.88b7c333.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.005248484675ba80964401204909c9b8.js"
+  "./precache-manifest.d40469d375f7d17dbeddcb29266a00c6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
