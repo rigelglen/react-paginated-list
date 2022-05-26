a728632f6c68270595963fc20a2491d862c0b171
rigelglen <rigelglen@gmail.com>
2022-05-26 09:44:36 +0000

    Updates

---
+++ precache-manifest.00b184f9d1b2380943950f3e50764d4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a18eb67025e6b7430ac1b5a7c01c6e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d08c05fc00c6ef5a008d",
+    "url": "./static/css/main.b5ad3dfe.chunk.css"
+  },
+  {
+    "revision": "083e77ca9665a0eb7f3a",
+    "url": "./static/js/2.1b7ecdfa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.1b7ecdfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d08c05fc00c6ef5a008d",
+    "url": "./static/js/main.6befe482.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "./static/js/main.6befe482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8281b323063a3ee29fb3",
+    "url": "./static/js/runtime-main.88b7c333.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.52a222472fd504a59b8c86ee8577da85.js"
+  "./precache-manifest.00b184f9d1b2380943950f3e50764d4e.js"
 );
 
 self.addEventListener('message', (event) => {
